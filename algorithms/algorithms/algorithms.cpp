f51eaca9ad107bb5a2f9b9f38575d2529df4ed15
f-murphy <100728686+f-murphy@users.noreply.github.com>
2022-10-29 03:49:22 +0300

    initial project

---
+++ algorithms/algorithms/algorithms.cpp
@@ -0,0 +1,13 @@
+﻿// algorithms.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
+
