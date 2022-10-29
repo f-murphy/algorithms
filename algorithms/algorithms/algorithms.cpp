83b283b772a30a01f784473b1fcaa818423dda54
f-murphy <100728686+f-murphy@users.noreply.github.com>
2022-10-29 04:27:44 +0300

    add quickSort

--- algorithms/algorithms/algorithms.cpp
+++ algorithms/algorithms/algorithms.cpp
@@ -1,13 +1,64 @@
-﻿// algorithms.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
-
+﻿#include <iostream>
+#include <algorithm>
 using namespace std;
+
+int partition(int a[], int start, int end)
+{
+    // Выбираем крайний правый элемент в качестве опорного элемента массива
+    int pivot = a[end];
+
+    // элементы, меньшие точки поворота, будут перемещены влево от `pIndex`
+    // элементы больше, чем точка поворота, будут сдвинуты вправо от `pIndex`
+    // равные элементы могут идти в любом направлении
+    int pIndex = start;
+
+    // каждый раз, когда мы находим элемент, меньший или равный опорному, `pIndex`
+    // увеличивается, и этот элемент будет помещен перед опорной точкой.
+    for (int i = start; i < end; i++)
+    {
+        if (a[i] <= pivot)
+        {
+            swap(a[i], a[pIndex]);
+            pIndex++;
+        }
+    }
+
+    // поменять местами `pIndex` с пивотом
+    swap(a[pIndex], a[end]);
+
+    // вернуть `pIndex` (индекс опорного элемента)
+    return pIndex;
+}
+
+// Процедура быстрой сортировки
+void quicksort(int a[], int start, int end)
+{
+    // базовое условие
+    if (start >= end) {
+        return;
+    }
+
+    // переставить элементы по оси
+    int pivot = partition(a, start, end);
+
+    // повторяем подмассив, содержащий элементы, меньшие опорной точки
+    quicksort(a, start, pivot - 1);
+
+    // повторяем подмассив, содержащий элементы, превышающие точку опоры
+    quicksort(a, pivot + 1, end);
+}
 
 int main()
 {
-    cout << "Hello World!\n";
+    int array[] = { 9, -3, 5, 2, 6, 8, -6, 1, 5, 9,-2, 3 };
+    int n = sizeof(a) / sizeof(a[0]);
+
+    quicksort(array, 0, n - 1);
+
+    // печатаем отсортированный массив
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+
+    return 0;
 }
-
-
